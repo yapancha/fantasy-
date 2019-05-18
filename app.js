c6279ea1b03caca4cfe03ecbb77422f95b67c0c5
mikeng30 <panchalyash77@gmail.com>
2019-05-18 16:03:26 -0400

    upload players sucessful

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ let mysql = require("mysql2");
 let bodyParser = require("body-parser");
 var request = require("request");
 var cricapi = require("cricapi");
+var path = require("path");
+const fs = require("fs");
 const app = express();
-cricapi.setAPIKey("mA2JflRG73h1887W1FzSFTTBv0k2");
-cricapi.matches(function(databundle) {
-  console.log("matches", databundle);
-});
+
+app.use("*/css", express.static("public/css"));
+app.use("*/js", express.static("public/js"));
+app.use("*/images", express.static("public/images"));
+app.use("*/icons", express.static("public/icons"));
+app.use("*/partials", express.static("views/partials"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+let rawdata = fs.readFileSync("plyers.json");
+let players = JSON.parse(rawdata);
+
+// cricapi.setAPIKey("mA2JflRG73h1887W1FzSFTTBv0k2");
+// cricapi.matches(function(databundle) {
+//   console.log("matches", databundle);
+// });
 // info for mysql connection
 var connection = mysql.createConnection({
   host: "remotemysql.com",
@@ -21,38 +35,44 @@ connection.connect(function(err) {
     console.log(err);
     return;
   }
-
   console.log("You are now connected with mysql database...");
 });
 
-app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    // to support URL-encoded bodies
     extended: true
   })
 );
 
-//login
-app.get("/users/login/:username", function(req, res) {
-  console.log("GET login user");
-  var username = req.params.username;
-  console.log(username);
+app.get("/players", (req, res) => {
+  res.render("sample.ejs");
+});
+app.get("/players/find/:name", (req, res) => {
   connection.execute(
-    `SELECT username, password FROM users WHERE username = ?`,
-    [username],
+    "SELECT `full_name`, `country` FROM `players` WHERE `full_name` like ?  ",
+    ["%" + req.params.name + "%"],
     function(error, results, fields) {
       if (error) throw error;
-      request(
-        "https://cricapi.com/api/playerStats?apikey=mA2JflRG73h1887W1FzSFTTBv0k2&pid=35320",
-        function(error, response, body) {
-          console.log("error:", error); // Print the error if one occurred
-          console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-          res.json({ results, body: JSON.parse(body) });
-        }
-      );
+      res.send(results);
     }
   );
+});
+//login
+app.get("/players/add", function(req, res) {
+  console.log("GET login user");
+
+  players.forEach(function(player) {
+    connection.execute(
+      "INSERT INTO `players`(`player_id`, `full_name`, `country`) VALUES (?,?,?)",
+      [player.pid, player.name, player.country],
+      function(error, results, fields) {
+        if (error) throw error;
+      }
+    );
+  });
+
+  res.send("done");
 });
 
 //signup
---
+++ public/js/sample.js
@@ -0,0 +1,28 @@
+var x = document.getElementById("playerFind");
+
+x.addEventListener("input", function() {
+  document.getElementById("demo").innerHTML = "You wrote: " + x.value;
+  var resultElement = document.getElementById("demo2");
+
+  axios
+    .get("/players/find/" + x.value)
+    .then(function(response) {
+      let len = response.data.length;
+      if (len == 0) {
+        resultElement.innerHTML = "No players found";
+      } else {
+        resultElement.innerHTML = "";
+      }
+      for (var i = 0; i < len; i++) {
+        resultElement.innerHTML +=
+          "<strong>Name:</strong> " +
+          response.data[i].full_name +
+          "  <strong>Country: </strong>: " +
+          response.data[i].country +
+          "<br>";
+      }
+    })
+    .catch(function(error) {
+      resultElement.innerHTML = error;
+    });
+});
